f8e79a53335c9dcf6a0d5e49a522659527d596f0
gift2021 <1467722467@qq.com>
2024-06-04 22:46:27 +0800

    哈哈哈哈哈，累成狗

--- backTest.py
+++ backTest.py
@@ -6,7 +6,7 @@ ts.set_token('6a340aafdbb9232fbbbab2adf6ec0350bd70bfd87a6bf588972b323e')
 # ts.set_token('303ece62497562396a9420fb0a36201d99f1ba1bd6c7713391a5a041')
 pro = ts.pro_api()
 
-def bkt(ts_code,start_date,end_date,buy,sell):
+def bkt(ts_code,start_date,end_date):
     '''
     简单回测系统，输入代码，开始，结束日期，买入价，卖出价，返回回测收益，模拟资金10w，无手续费,全仓买入模式
     :param ts_code:股票代码
@@ -16,54 +16,89 @@ def bkt(ts_code,start_date,end_date,buy,sell):
     :param sell:卖出价
     :return:交易记录，获利
     '''
+    trade = []
+
+
+
+    df = pro.daily(ts_code=ts_code, start_date=start_date, end_date=end_date)
+    min = 10000
+    max = 0
+    for i in range(len(df) - 1, -1, -1):
+         # df.iloc[i,2]好像是开盘价，还不是最低价
+        price = df.iloc[i, 2]
+        if max < price:
+             max = price
+        if min > price:
+             min = price
+    list = []
+    add_price = (max - min) / 5
+    for i in range(5):
+        list.append(min + add_price * i)
+
+    list.append(max)
 
     # 返回record 记录交易记录
     # 格式：time
-    record = ''
 
-    #hold=1为持有，-1为未持有
-    hold = -1
+    maxprofit = 0
+
+    flagrecord = ''
+
+    flagbuyprice = 0
+
+    flagsellprice = 0
 
     #初始收益为0
     benfite = 0
-
-    #初始本金为10w
-    num = 100000
-
-    #股票数量
-    stack_num = 0
-    price = 0
-
     # 交易次数
     flag = 0
 
-    df = pro.daily(ts_code=ts_code, start_date=start_date, end_date=end_date)
+    for j in range(5):
+        for k in range(5 - j):
+            record = ''
+            # hold=1为持有，-1为未持有
+            hold = -1
+            # 初始本金为10w
+            num = 100000
+            # 股票数量
+            stack_num = 0
+            price = 0
+            for i in range(len(df)-1,-1,-1):
+                price = df.iloc[i, 2]
+                # 卖出价格
+                if price >= list[5-k]:
+                    if hold == 1:
+                        num = num + stack_num*price
+                        flag += 1
+                        record += str(flag) + ':sell   time:' + df.iloc[i,1] + '   price:' + str(price) + '\n'
+                        stack_num = 0
+                        hold = -hold
 
-    for i in range(len(df)-1,-1,-1):
-        price = df.iloc[i, 2]
-        if price >= sell:
-            if hold == 1:
-                num = num + stack_num*price
-                flag += 1
-                record += str(flag) + ':sell   time:' + df.iloc[i,1] + '   price:' + str(price) + '\n'
-                stack_num = 0
-                hold = -hold
+
+                else:
+                    if price <= list[j]:
+                        if hold == -1:
+                            stack_num = (num//(price*100))*100
+                            num = num - stack_num * price
+                            hold = -hold
+                            flag += 1
+                            record += str(flag) + ':buy   time:' + df.iloc[i, 1] + '   price:' + str(price) + '   stockNum    ' +str(stack_num) + '\n'
+            profit = round(num + stack_num*price -100000)
+            if(maxprofit < profit):
+                flagbuyprice = list[j]
+                flagsellprice = list[5-k]
+                maxprofit = profit
+                flagrecord = record
+    trade.append(ts_code)
+    trade.append(flagbuyprice)
+    trade.append(flagsellprice)
+    trade.append(maxprofit)
 
 
-
-        else:
-            if price <= buy:
-                if hold == -1:
-                    stack_num = (num//(price*100))*100
-                    num = num - stack_num * price
-                    hold = -hold
-                    flag += 1
-                    record += str(flag) + ':buy   time:' + df.iloc[i, 1] + '   price:' + str(price) + '   stockNum    ' +str(stack_num) + '\n'
-    profit = round(num + stack_num*price -100000)
-    return record,profit
+    return trade
 
 if __name__ == '__main__':
-    record,profit = bkt('000020.SZ','20230101', '20240601',11.118,13.315999999)
-    print(record)
-    print(profit)
+    trade = bkt('000058.SZ','20180101', '20230606')
+    print(trade)
+
 
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import priceIoc as pic
 df = pd.read_csv('D:\ZA\data\stock\stockList.csv')
 stockList = pd.DataFrame(df)
 
-bigProfitList = [['000001.SZ',16.21,16.21,4085]]
+bigProfitList = [[]]
 
 def test(start_date,end_date):
     '''
@@ -23,42 +23,18 @@ def test(start_date,end_date):
     flagrecord = ''
     flagpro = 0
     for i in range(len(stockList)):
-        # 买入和卖出价在一个列表中，对日期内价格进行切分
-        # todo 这里调用一次接口，计算价格区间
-        tradeList = pic.priceIoc(stockList.iloc[i,1],start_date,end_date,5)
-
-        if (sum(tradeList)/5) > 20:
-            continue
-
-        # # 对切分的价格进行遍历，查找获利最多的价格区间
-        for j in range(len(tradeList)):
-            max = 0
-            for k in range(5-j-1):
-
-                if(j>k+1):
-                    break
-                # 获取回测结果
-                # todo 这里调用一次接口
-                record , profit = bkt.bkt(str(stockList.iloc[i,1]),start_date,end_date,tradeList[j],tradeList[k+1])
-
-                if profit>max:
-                    max = profit
-                    flagj = j
-                    flagk = k
-                    flagrecord = record
-                    flagpro = profit
-
-
-        bigProfitList.append([stockList.iloc[i][1],tradeList[flagj],tradeList[flagk],round(flagpro)])
-        res += str(stockList.iloc[i, 1]) + '买入，卖出价   ' + str(tradeList[flagj]) + '   ' + str(tradeList[flagk + 1]) \
-               + '   获利  ' + str(flagpro) + '\n' + flagrecord
-
-        if(i == 50):
+        # bigProfitList[i] = bkt.bkt(str(stockList.iloc[i, 1]),'20230101', '20240601')
+        bigProfitList.append(bkt.bkt(str(stockList.iloc[i, 1]),'20230101', '20240601'))
+        if (i > 4500):
             break
         print(i)
-    return res,bigProfitList
+    return bigProfitList
 
-res,bigProfitList = test('20230101', '20240601')
+bigProfitList = test('20230101', '20240601')
+
+print(bigProfitList)
+
+
 bigProfitList = pd.DataFrame(bigProfitList)
 
 bigProfitList = bigProfitList.drop(0)
@@ -66,9 +42,7 @@ bigProfitList = bigProfitList.drop(0)
 bigProfitList.sort_values(by=bigProfitList.columns[3], ascending=False, inplace=True)
 print(bigProfitList)
 bigProfitList.to_csv('output.csv', index=False)  # 保存到CSV文件
-# todo  将排序后的交易记录拿出来，怎么存储是一个问题
 
-# 先跑个100股，看看效果
 
 
 
